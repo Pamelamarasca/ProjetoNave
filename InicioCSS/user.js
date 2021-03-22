9a108000577a21564213f2615dfaab4a9319b0f2
Pamela Marasca <pamelamarascasilva@gmail.com>
2021-03-22 10:41:30 -0300

    aula pt.6

--- InicioCSS/user.js
+++ InicioCSS/user.js
@@ -23,3 +23,69 @@ console.log(usuário['nome'])
 console.log(usuário['endereco']['rua'])
 console.log(usuário['endereco']['numero'])
 usuário.func([])
+
+const user = {
+    nome: 'Usuário',
+    idade: 25,
+    endereco: {
+        rua: 'Rua central',
+        numero: 1
+    },
+    projetos: ['Projeto 1', 'Projeto 2']
+}
+
+
+const students = [
+    { id: 1, name: 'John Doe', age: 23, scholarship: false },
+    { id: 2, name: 'Margoe Rose', age: 19, scholarship: true },
+    { id: 3, name: 'Kayle', age: 22, scholarship: true }
+]
+
+
+console.log('------user2-----')
+
+const user = {
+    nome: 'Usuário',
+    idade: 25,
+    endereco: {
+        rua: 'Rua central',
+        numero: 1
+    },
+    projetos: ['Projeto 1', 'Projeto 2']
+}
+
+
+const students = [
+    { id: 1, name: 'John Doe', age: 23, scholarship: false },
+    { id: 2, name: 'Margoe Rose', age: 19, scholarship: true },
+    { id: 3, name: 'Kayle', age: 22, scholarship: true }
+]
+
+//1a
+const { nome } = user
+console.log(nome)
+
+// 1b
+const { endereco: { rua } } = user
+console.log(rua)
+
+// 1c
+const { projetos } = user
+console.log(projetos)
+
+// 1d
+const { projetos: [, proj] } = user
+console.log(proj)
+
+
+// 2a
+const [first] = students
+console.log(first)
+
+// 2b
+const [, , third] = students
+console.log(third)
+
+// 2c
+const [, { name }] = students
+console.log(name)
